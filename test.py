333ba84bf378855ce5257055ef659775787b525a
1907FB <994876761@qq.com>
2025-04-05 20:47:17 +0800

    .

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import torch
 import torch.nn as nn
 if __name__ == "__main__":
-    latency_accumulation = torch.load("./llama-2-7b-ILP-test_la.pth")
-    print(latency_accumulation.keys())
+    grad_stat = torch.load("./llama2-7b-grad.pth")
+    print(grad_stat.keys())
